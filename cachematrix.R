c818e2794e83d58a64bb402efced658cad64f895
madhurnitc <madhurnitc@gmail.com>
2015-07-22 00:28:15 -0400

    added the code for cached matrix calculation

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,28 @@
 
 ## Write a short comment describing this function
 
+## This function contains the following functions:
+## set,get,setInverse and getInverse
+## We have used the <<- operator  
+#  to assign a value to an object 
+## in an environment that is different from the current environment
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # Create a "empty" inverse Matrix.This will be the place holder for the x'inverse matrix
+  xInverse <- NULL
+  # setter fucntion to set the matrix to be used by makeCacheMatrix function.
+  set <- function(y){
+    x <<- y
+    xInverse <<- NULL
+  }
+  # getter function to return the matrix that is being used by makeCacheMatrix fucntion.
+  get <- function() x
+  # setter function to set the inverse 
+  setInverse <- function(inverse) xInverse <<- inverse
+  # getter function to set the inverse 
+  getInverse <- function() xInverse
+  # add these method to the environment
+  list(set = set , get = get, setInverse = setInverse , getInverse = getInverse)
 
 }
 
@@ -12,4 +33,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   
+   inverseMatrix <- x$getInverse()
+   ## If the inverse exists , then return from the cache
+   if (!is.null(inverseMatrix))
+   {
+      message("Inverse exists in cache")
+      return(inverseMatrix)
+   }
+   ## otherwise calculate the inverse  
+   message("Calculating the inverse")
+   ## get the matrix of which inverse is to be calculated.
+   matrix <- x$get()
+   ## Calculate the inverse of the above matrix.
+   inverseMatrix <- solve(matrix)
+   ## Cache the inverse matrix for future calculation.
+   message("Caching the inverse")
+   x$setInverse(inverseMatrix)
+   inverseMatrix
 }
+
+# Test
+# generate a random square, non-singular matrix
+test <- matrix(runif(9,1,100),3,3)
+# generate the makeCacheMatrix object with this matrix
+testCached <- makeCacheMatrix(test)
+# from now on calculate or retrieve calculated inversion using the cacheSolve function
+
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
